b472746e861c0b9758dabe73ab515a778c66017f
Andrei Herdt <andrei.herdt@dlr.de>
2012-08-17 11:14:25 +0200

    Remove obsolete files

--- unittest/use-solver/mexit.m
+++
@@ -1,5 +0,0 @@
-% ATT: this links statically against the mpc-walkgen.lib and dynamically
-% against mpc-walkgen.dll
-copyfile('D:\andrei\mpc-walkgen\build-new\Debug\mpc-walkgen.dll','D:\andrei\mpc-walkgen\unittest\use-solver\mpc-walkgen.dll')
-copyfile('D:\andrei\mpc-walkgen\build-new\Debug\mpc-walkgen.lib','D:\andrei\mpc-walkgen\unittest\use-solver\mpc-walkgen.lib')
-mex -g -D_DEBUG -v -ID:\andrei\mpc-walkgen\include -ID:\andrei\mpc-walkgen\deps\eigen\ -LD:\andrei\mpc-walkgen\build-new\Debug\ walkgen_cpp.cpp mpc-walkgen.lib
