75442b4716f8fe8b41ed9e4b731a03d9f41359e8
Andrei Herdt <andrei.herdt@dlr.de>
2013-01-15 16:20:54 +0100

    Changes in makefiles

--- interfaces/matlab/simulink/make_debug.m
+++ interfaces/matlab/simulink/make_debug.m
@@ -5,7 +5,7 @@
 %%
 
 if (ispc == 0)
-    CFLAGS  = [ '-lblas -lstdc++ -D__cpluplus -D__MATLAB__ -O -DLINUX', ' ' ];
+    CFLAGS  = [ '-lblas -lstdc++ -D__cpluplus -D__MATLAB__ -O -D__LINUX__', ' ' ];
 else
     CFLAGS  = ['-v', ' -D__WIN32__',' -D__NO_COPYRIGHT__',' -D__SUPPRESSANYOUTPUT__',' -D__STDC__', ' ' ]; 
 end
--- interfaces/matlab/simulink/make_release.m
+++ interfaces/matlab/simulink/make_release.m
@@ -5,7 +5,7 @@
 %%
 
 if (ispc == 0)
-    CFLAGS  = ['-lblas -lstdc++ -D__cpluplus -D__MATLAB__ -O -DLINUX', ' ' ]; %% -D__NO_COPYRIGHT__ -D__SUPPRESSANYOUTPUT__
+    CFLAGS  = ['-lblas -lstdc++ -D__cpluplus -D__MATLAB__ -O -D__LINUX__', ' ' ]; %% -D__NO_COPYRIGHT__ -D__SUPPRESSANYOUTPUT__
 else
     CFLAGS  = [' -D__WIN32__',' -D__NO_COPYRIGHT__',' -D__SUPPRESSANYOUTPUT__',' -D__STDC__',' -O', ' ' ]; 
 end
